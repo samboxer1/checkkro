7ba89d1409c636b4b3459c5c78c07b9dfb061fbd
samboxer1 <sameervermasv89@gmail.com>
2024-07-24 15:48:39 +0530

    Add files via upload
    this file contains a cpp program describing the average of two numbers

---
+++ git.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,avg;
+    cout<<"enter two no";
+    cin>>a;
+    cin>>b;
+
+    avg=(a+b)/2;
+    cout<<"avg of two no is "<<avg;
+}
